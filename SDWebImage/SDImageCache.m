1f31b91d7caa3fb9b4b0860080aaf71d9697e2d4
Daniel Tsirulnikov <daniel.tskv@gmail.com>
2017-08-01 04:35:14 +0300

    Merge branch 'master' of https://github.com/rs/SDWebImage

--- SDWebImage/SDImageCache.m
+++ SDWebImage/SDImageCache.m
@@ -340,8 +340,7 @@ FOUNDATION_STATIC_INLINE NSUInteger SDCacheCostForImage(UIImage *image) {
             image = [UIImage decodedImageWithImage:image];
         }
         return image;
-    }
-    else {
+    } else {
         return nil;
     }
 }
